1ed835e016bfd921c137ba39a045aa62116117fa
Abdallah Ali <amaali1991@gmail.com>
2024-08-23 13:27:43 +0300

    lazygit keymap

--- modules/nixvim/lazygit/default.nix
+++ modules/nixvim/lazygit/default.nix
@@ -1,23 +1,37 @@
 { helpers, ... }: {
+  keymaps = [
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      action = "<cmd>LazyGit<cr>";
+      key = "<leader>gg";
+      options = {
+        desc = "Open LazyGit";
+        silent = true;
+        noremap = true;
+      };
+    }
+  ];
   plugins.lazygit = {
-		enable = true;
-		settings = {
-			floating_window_scaling_factor = 0.9;
-  floating_window_use_plenary = false;
-  floating_window_winblend = 0;
-  use_custom_config_file_path = false;
-  use_neovim_remote = true;
-			floating_window_border_chars = [
-  "╭"
-  "─"
-  "╮"
-  "│"
-  "╯"
-  "─"
-  "╰"
-  "│"
-];
-
-};
-	};
+    enable = true;
+    settings = {
+      floating_window_scaling_factor = 0.9;
+      floating_window_use_plenary = false;
+      floating_window_winblend = 0;
+      use_custom_config_file_path = false;
+      use_neovim_remote = true;
+      floating_window_border_chars = [
+        "╭"
+        "─"
+        "╮"
+        "│"
+        "╯"
+        "─"
+        "╰"
+        "│"
+      ];
+    };
+  };
 }
