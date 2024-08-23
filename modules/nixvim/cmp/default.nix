0f084a2f8b8ea088ce7cc345b8a0cee21875a36e
Abdallah Ali <amaali1991@gmail.com>
2024-08-23 12:23:48 +0300

    fixing on going cmp!!!

--- modules/nixvim/cmp/default.nix
+++ modules/nixvim/cmp/default.nix
@@ -31,44 +31,47 @@
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-e>" = "cmp.mapping.abort()";
-          "<CR>" = helpers.mkRaw # lua
-            ''
-              							cmp.mapping({
-              								i = function(fallback)
-              									if cmp.visible() and cmp.get_active_entry() then
-              										cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-              									else
-              										fallback()
-              									end
-              								end,
-              								s = cmp.mapping.confirm({ select = true }),
-              								c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-              							})
-              						'';
-          "<Tab>" = helpers.mkRaw # lua
-            ''
-              						cmp.mapping(function(fallback)
-              							if cmp.visible() then
-                            		cmp.select_next_item()
-                            	elseif luasnip.locally_jumpable(1) then
-                            		luasnip.jump(1)
-                            	else
-                            		fallback()
-                            	end
-                            end, { "i", "s" })
-            '';
-          "<S-Tab>" = helpers.mkRaw # lua
-            ''
-              cmp.mapping(function(fallback)
-              	if cmp.visible() then
-              		cmp.select_prev_item()
-              	elseif luasnip.locally_jumpable(-1) then
-              		luasnip.jump(-1)
-              	else
-              		fallback()
-              	end
-              end, { "i", "s" })
-            '';
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          # "<CR>" =
+          #   ''
+          #     							cmp.mapping({
+          #     								i = function(fallback)
+          #     									if cmp.visible() and cmp.get_active_entry() then
+          #     										cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          #     									else
+          #     										fallback()
+          #     									end
+          #     								end,
+          #     								s = cmp.mapping.confirm({ select = true }),
+          #     								c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+          #     							})
+          #     						'';
+          # "<Tab>" =
+          #   ''
+          #     						cmp.mapping(function(fallback)
+          #     							if cmp.visible() then
+          #                   		cmp.select_next_item()
+          #                   	elseif luasnip.locally_jumpable(1) then
+          #                   		luasnip.jump(1)
+          #                   	else
+          #                   		fallback()
+          #                   	end
+          #                   end, { "i", "s" })
+          #   '';
+          # "<S-Tab>" =
+          #   ''
+          #     cmp.mapping(function(fallback)
+          #     	if cmp.visible() then
+          #     		cmp.select_prev_item()
+          #     	elseif luasnip.locally_jumpable(-1) then
+          #     		luasnip.jump(-1)
+          #     	else
+          #     		fallback()
+          #     	end
+          #     end, { "i", "s" })
+          #   '';
         };
 
         snippet = {
@@ -89,14 +92,14 @@
           }
           { name = "omni"; }
           { name = "path"; }
-          { name = "cmdline"; }
+          # { name = "cmdline"; }
           { name = "buffer"; }
           { name = "dictionary"; }
         ];
       };
     };
 
-    cmp-cmdline.enable = true;
+    # cmp-cmdline.enable = true;
     cmp-dictionary.enable = true;
     cmp-fuzzy-path.enable = true;
     cmp-fuzzy-buffer.enable = true;
