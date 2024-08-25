f450924347c7a73ac897a740e11d929755ba1527
Abdallah Ali <amaali1991@gmail.com>
2024-08-25 18:15:27 +0300

    colorscheem

--- modules/nixvim/colorscheme/default.nix
+++ modules/nixvim/colorscheme/default.nix
@@ -1,22 +1,5 @@
 { helpers, ... }:
 {
-  # colorschemes.oxocarbon.enable = true;
-	# extraConfigLuaPost = # lua
-  #   ''
-  #     do
-  #       -- The NixVim Nord colorscheme doesn't assign a pleasing color to the window
-  #       -- separator by default. Here we override it with a softer color.
-  #       local colors = require("nord.colors")
-  #       vim.api.nvim_set_hl(0,  "WinSeparator", {
-  #         fg = colors.nord2_gui,
-  #       })
-  #     end
-  #   '';
+  colorschemes.oxocarbon.enable = true;
 
-colorschemes.onedark = {
-    enable = false;
-    settings = {
-			style = "warmer";
-    };
-  };
 }
