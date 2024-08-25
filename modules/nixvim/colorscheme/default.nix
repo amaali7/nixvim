d986e29838638ddcee9659c6ffe25a9e134760ef
Abdallah Ali <amaali1991@gmail.com>
2024-08-25 17:40:36 +0300

    theme changed

--- modules/nixvim/colorscheme/default.nix
+++ modules/nixvim/colorscheme/default.nix
@@ -1,19 +1,20 @@
 { helpers, ... }:
 {
-  extraConfigLuaPost = # lua
-    ''
-      do
-        -- The NixVim Nord colorscheme doesn't assign a pleasing color to the window
-        -- separator by default. Here we override it with a softer color.
-        local colors = require("nord.colors")
-        vim.api.nvim_set_hl(0,  "WinSeparator", {
-          fg = colors.nord2_gui,
-        })
-      end
-    '';
+	colorschemes.oxocarbon.enable = true;
+	# extraConfigLuaPost = # lua
+  #   ''
+  #     do
+  #       -- The NixVim Nord colorscheme doesn't assign a pleasing color to the window
+  #       -- separator by default. Here we override it with a softer color.
+  #       local colors = require("nord.colors")
+  #       vim.api.nvim_set_hl(0,  "WinSeparator", {
+  #         fg = colors.nord2_gui,
+  #       })
+  #     end
+  #   '';
 
   colorschemes.nord = {
-    enable = true;
+    enable = false;
     settings = {
       borders = true;
       contrast = false;
