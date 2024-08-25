d04ab6577cb94cfaaffa913262ef9d0b2bf5569a
Abdallah Ali <amaali1991@gmail.com>
2024-08-25 12:35:20 +0300

    disable lua script

--- modules/nixvim/auto-session/default.nix
+++ modules/nixvim/auto-session/default.nix
@@ -14,11 +14,11 @@
       createEnabled = true;
       useGitBranch = true;
     };
-		extraOptions = {
-			__raw = ''
-				vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-			'';
-		};
+		#	extraOptions = {
+		#	e	__raw = ''
+		#	e		vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+		#	e	'';
+		#	e};
   };
 
   keymaps = [
